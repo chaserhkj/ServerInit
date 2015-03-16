ac8dbb1ba8ab16f33af85e78a11cb37673a06fcb
HuangKangJing <huangkangjing@gmail.com>
2015-03-16 16:15:59 +0800

    Updated meta data

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+setup(name='MCServerInit',
+      version='0.1',
+      description='Application to create Aliyun instances for running MC on demand.',
+      author='Chaserhkj',
+      author_email='me@chaserhkj.tk',
+      url='http://www.chaserhkj.tk'
      )
